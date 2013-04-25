fd9fe77b2a6158830be004af35816b5f125c8043
Karl Entwistle <karl.entwistle@yourgolftravel.com>
2013-04-25 14:03:35 +0100

    forgot my writer

--- serveip.go
+++ serveip.go
@@ -15,7 +15,7 @@ func main() {
 func serveIP(w http.ResponseWriter, r *http.Request) {
   if len(r.Header.Get("X-Forwarded-For")) > 0 {
     ips := strings.Split(r.Header.Get("X-Forwarded-For"), ", ")
-    fmt.Fprintf(ips[(len(ips) - 1)])
+    fmt.Fprintf(w, ips[(len(ips) - 1)])
   } else {
     ips := strings.Split(r.RemoteAddr, ":")
     fmt.Fprintf(w, ips[0])
